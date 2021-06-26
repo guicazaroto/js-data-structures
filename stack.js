f706e50e569530735b9e924f765e96edc4e23759
Guilherme Cazaroto <gui.cazaroto@gmail.com>
2021-06-26 00:00:29 -0300

    feat: stack

--- stack.js
+++ stack.js
@@ -1,7 +1,39 @@
+
 class Stack {
+
   constructor() {
-    this.items = []
+    this.mapInstance = new WeakMap()
+    this.clear()
   }
 
-  
+  push(item) {  
+    this.items.push(item)
+  }
+
+  pop () {  
+    this.items.pop()
+  }
+
+  peek () {  
+    const lastItemIndex = this.size() - 1
+    return this.items[lastItemIndex]
+  }
+
+  isEmpty() {
+    return this.size() === 0
+  }
+
+  clear () {
+    this.mapInstance.set(this, [])
+    this.items = this.mapInstance.get(this)
+  }
+
+  size () {  
+    return this.items.length
+  }
 }
+
+const stack = new Stack()
+const stack2 = new Stack()
+stack.push('hello')
+stack2.push('world')
